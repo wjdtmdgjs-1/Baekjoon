f14a8a1f61c3d9301fa5741a50bbb9953b59036c
wjdtmdgjs-1 <jeong5207@naver.com>
2024-08-24 02:03:07 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 2.16 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -6,18 +6,19 @@ class Solution {
         List<String> aa = new ArrayList<>(Arrays.asList(s.split("")));
         List<String> bb = new ArrayList<>();
         
-        
-        if(aa.size()%2==0){
+        if(aa.size()!=1){
+            if(aa.size()%2==0){
                 bb.add(aa.get((aa.size()/2)-1));
                 bb.add(aa.get(aa.size()/2));
-        }else{
+            }else{
                 bb.add(aa.get(aa.size()/2));
-        }
+            }
             
-        for (String item : bb) {
+             for (String item : bb) {
                 answer += item;
-        }
-        
+            }
+            
+        }else{ answer =s;}
 
 
         return answer;
