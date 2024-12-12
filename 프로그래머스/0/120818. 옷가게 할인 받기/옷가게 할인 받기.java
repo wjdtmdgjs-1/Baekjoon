83f54cca6d1c2737468c9357fd37f1c3feb68ba6
wjdtmdgjs <jeong5207@naver.com>
2024-12-12 19:12:41 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if(price<100000){
+            answer=price;
+        }
+        else if(price<300000){
+            answer=(int)(price*0.95);
+        }
+        else if(price<500000){
+            answer=(int)(price*0.9);
+        }
+        else {
+            answer=(int)(price*0.8);
+        }
+        return answer;
+    }
+}
