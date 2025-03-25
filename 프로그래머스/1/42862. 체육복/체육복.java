105f908f44b6ec1519b9a42229147bc53edcac11
wjdtmdgjs <jeong5207@naver.com>
2025-03-25 19:35:13 +0900

    [level 1] Title: 체육복, Time: 0.59 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,37 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 초기 answer에 체육복을 도난당하지 않은 학생 수를 구한다.
-        int answer = n - lost.length;
+        int len = lost.length;
+        int answer= n - len;
+        Set<Integer> clothes = new HashSet<Integer>();
+        boolean[] visited = new boolean[len];
         
-        // reserve 배열을 정렬한다.
-        Arrays.sort(reserve);
-        // lost 배열을 정렬한다.
-        Arrays.sort(lost);
-        
-        // 반복문을 통해 여벌의 옷을 가져왔지만 도난당한 학생 수를 계산한다.
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
+        for(int num : reserve) {
+            clothes.add(num);
         }
         
-        // 반복문을 통해 도난당했지만 여벌의 체육복을 빌릴 수 있는 학생 수를 계산한다.
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
-                    answer++;
-                    reserve[j] = -1;
-                    break;
-                }
+        Arrays.sort(lost);
+        for(int i = 0; i < len; i++) {
+            if(clothes.contains(lost[i])) {
+                answer++;
+                visited[i] = true;
+                clothes.remove(lost[i]);
             }
-        }		
-        
+        }
+
+
+        for(int i = len - 1; i >= 0; i--) {
+            if (visited[i]) continue;
+            
+            if(clothes.contains(lost[i] + 1)) {
+                clothes.remove(lost[i] + 1);
+                answer++;
+            } else if(clothes.contains(lost[i] - 1)) {
+                clothes.remove(lost[i] - 1);
+                answer++;
+            }
+        }
+
+
         return answer;
     }
 }
